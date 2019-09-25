e30792f259810764faffc3dc74f3bce966d39312
Bradley <bradleyhills@gmail.com>
2019-09-25 16:09:29 -0400

    First draft Hello world lol

---
+++ MFPG.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magic_Future_Predicting_Genie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
