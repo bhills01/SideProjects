167386be536cf360269868339872202f68ecdfca
Bradley <bradleyhills@gmail.com>
2019-09-27 16:28:57 -0400

    First draft - Brad and Brandon and friends

--- MFPG/MFPG/Program.cs
+++ MFPG/MFPG/Program.cs
@@ -6,7 +6,51 @@ namespace MFPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tech Elevator");
+            Random randomNum = new Random();
+            string[] answer = new string[] { "Yes", "No", "Maybe", "Ask again Later", "Does not seem likely", "You can count on it!" };
+            string yesOrNo = "";
+
+                Console.WriteLine("*************************************************************************************************");
+                Console.WriteLine("*************************************************************************************************");
+                Console.WriteLine("*************************************************************************************************");
+                Console.WriteLine("Who dare disturb the Great Flambini All Powerful and Majestic Mystic of the Futuuuuurrrreeeee!!!!");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Well, since you have my attention...Ask me any yes or no question and I will tell your future!: "); Console.Read();
+                Console.WriteLine();
+                Console.WriteLine($"I have consorted with the spirits to divine your future! Here is your answer: {answer[randomNum.Next(0, 5)]}");
+                Console.ReadLine();
+             
+                Console.Write("Would you like to ask another question(y)es or (n)o? ");
+
+                yesOrNo = Console.ReadLine();
+
+            while (yesOrNo != "n")
+            {
+                Console.WriteLine("*************************************************************************************************");
+                Console.WriteLine("*************************************************************************************************");
+                Console.WriteLine("*************************************************************************************************");
+                Console.Write("Fine...What is your questions? "); Console.Read();
+                Console.WriteLine();
+                Console.WriteLine($"I have consorted with the spirits to divine your future! Here is your answer: {answer[randomNum.Next(0, 5)]}");
+                Console.ReadLine();
+
+                Console.Write("Would you like to ask another question(y)es or (n)o? ");
+
+                yesOrNo = Console.ReadLine();
+            }
+            
+            
+             Console.WriteLine("Thank god!!! Off with you then!");
+             Console.WriteLine("Tap (enter) to return to your boring life!");
+             Console.ReadLine();
+
+        
+            
+            
+            
         }
     }
 }
