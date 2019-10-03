5f215f232ce6e6b8e1f858f4e3fb00bcf529aaff
Glenn Weber <glennweber099@gmail.com>
2019-10-03 08:07:50 -0400

    updated intro

--- MFPG/MFPG/Program.cs
+++ MFPG/MFPG/Program.cs
@@ -72,7 +72,7 @@ namespace MFPG
             // Code to make a menu for a user interface (working with classes and stuff like that)
             bool visible = true;
             Console.BackgroundColor = ConsoleColor.DarkRed;
-            for (int i = 0; i < 75; i++)
+            for (int i = 0; i < 50; i++)
             {
                 //Press Ctrl + C to Quit
                 string alert = "************************************************************************************************************************" +
