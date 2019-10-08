7df3150006634f420162b3c298fcd5c29aa0ffc6
Glenn Weber <glennweber099@gmail.com>
2019-10-08 08:47:39 -0400

    Began a basic number guesser

---
+++ MFPG/MFPG/Models/NumberGuesser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFPG.Models
+{
+    public class NumberGuesser
+    {
+        public void numberGuesser()
+        {
+            Random rnd = new Random();
+            int flambiniNumber = rnd.Next(1,10);
+            Console.WriteLine("Please guess a number.");
+            string userInput = Console.ReadLine();
+            if (userInput == flambiniNumber.ToString())
+            {
+                Console.WriteLine("Congrats, you guessed my number. Press [ENTER] to continue");
+                Console.ReadLine();
+
+            }
+            if (userInput != flambiniNumber.ToString())
+            {
+                Console.WriteLine("Incorrect, guess again");
+            }
+        }
+    }
+}
+    
+        
+    
