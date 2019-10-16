c2e07d751062fdf27a6bc7e3d474fc9720d0178e
Glenn Weber <glennweber099@gmail.com>
2019-10-16 09:32:33 -0400

    Flambini has a face and is a full screen program now

--- MFPG/MFPG/Models/Credits.cs
+++ MFPG/MFPG/Models/Credits.cs
@@ -8,16 +8,16 @@ namespace MFPG.Models
     {
         public string Contributers()
         {
-            return "\n************************************************************************************************************************" +
-                   "\n************************************************************************************************************************" +
-                   "\n************************************************************************************************************************" +
-                   "\n The Great Flambini is brought to you by a collaborative effort from the following Tech Elevator Cohort 12 .NET Members: " +
-                   "\n Brandon \"Intellesense\" Best" +
-                   "\n Glenn \"The Builder\" Weber" +
-                   "\n Bradley \"Big Papa\" Hills" +
-                   "\n Reid \"The Golden Steed\" Larson" +
-                   "\n With a Special Thanks to  \"Magic\" Mike Morel for filling our brains with magic!" +
-                   "\n Thank you for visiting the Great Flambini";
+            return "****************************************************************************************************************************************************************************" +
+                    "\n****************************************************************************************************************************************************************************" +
+                    "\n****************************************************************************************************************************************************************************" +
+                   "\n\t\t\t The Great Flambini is brought to you by a collaborative effort from the following Tech Elevator Cohort 12 .NET Members: " +
+                   "\n\t\t\t Brandon \"Intellesense\" Best" +
+                   "\n\t\t\t Glenn \"The Builder\" Weber" +
+                   "\n\t\t\t Bradley \"Big Papa\" Hills" +
+                   "\n\t\t\t Reid \"The Golden Steed\" Larson" +
+                   "\n\t\t\t With a Special Thanks to  \"Magic\" Mike Morel for filling our brains with magic!" +
+                   "\n\t\t\t Thank you for visiting the Great Flambini";
         }
     }
 }
--- MFPG/MFPG/Program.cs
+++ MFPG/MFPG/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using MFPG.Models;
+using System.IO;
+using System.Linq;
 
 namespace MFPG
 {
@@ -15,10 +17,10 @@ namespace MFPG
             for (int i = 0; i < 16; i++)
             {
                 //Press Ctrl + C to Quit
-                string alert = "************************************************************************************************************************" +
-                    "\n************************************************************************************************************************" +
-                    "\n************************************************************************************************************************" +
-                    "\n\tWho dare disturb the Great Flambini All Powerful and Majestic Mystic of the Futuuuuurrrreeeee!!!!";
+                string alert = "****************************************************************************************************************************************************************************" +
+                    "\n****************************************************************************************************************************************************************************" +
+                    "\n****************************************************************************************************************************************************************************" +
+                    "\n\t\t\t\tWho dare disturb the Great Flambini All Powerful and Majestic Mystic of the Futuuuuurrrreeeee!!!!";
                 Console.ForegroundColor = visible ? ConsoleColor.White: ConsoleColor.Black;
                 visible = !visible;
                 Console.Clear();
@@ -27,106 +29,117 @@ namespace MFPG
                 Thread.Sleep(300);
             }
             Console.ForegroundColor = ConsoleColor.Black;
+            Directory.SetCurrentDirectory(@"../../..");
             Console.WriteLine();
-
-            bool menu = true;
-
-            while (menu == true)
+            string inputLine = "";
+            using (StreamReader sr = new StreamReader("flambiniface.txt"))
             {
-                Console.WriteLine("************************************************************************************************************************");
-                Console.WriteLine("\n************************************************************************************************************************");
-                Console.WriteLine("\n************************************************************************************************************************");
-                Console.Write(
-                    "\t\t\t\t1) Fortune Teller" +
-                    "\n\t\t\t\t2) Magic 8-Ball (SFW)" +
-                    "\n\t\t\t\t3) Magic 8-Ball (NSFW)" +
-                    "\n\t\t\t\t4) Riddle Giver" +
-                    "\n\t\t\t\t5) Rock Paper Scissors" +
-                    "\n\t\t\t\t6) Random Fact Giver" +
-                    "\n\t\t\t\t7) Paper Rock Scissors" +
-                    "\n\t\t\t\tQ) Quit" +
-                    "\n\n\n\t\t\t\tPlease choose an option: ");
-                String input = Console.ReadLine().ToLower().Trim();
-
-                if (input.Length == 0)
+                while (!sr.EndOfStream)
                 {
-                    Console.Clear();
-                    continue;
-
-                }
-                if (input.Length > 1)
-                {
-                    Console.Clear();
-                    continue;
-
-                }
-                input = input.Substring(0, 1);
-
-                if (input == "q")
-                {
-                    menu = false;
-                    break;
-                }
-                else if (input == "1")
-                {
-                    FortuneTeller fortuneTeller = new FortuneTeller();
- 
-                    fortuneTeller.fortuneTeller();
-                }
-                else if (input == "2")
-                {
-                    MagicEightBall_SFW_ magicEightBallSFW = new MagicEightBall_SFW_();
- 
-                    magicEightBallSFW.magicEightBallSFW();
-                }
-                else if (input == "3")
-                {
-                    MagicEightBall_NSFW_ magicEightBallNSFW = new MagicEightBall_NSFW_();
-
-                    magicEightBallNSFW.magicEightBallNSFW();
-
-                }
-                else if (input == "4")
-                {
-                    Riddles riddle = new Riddles();
-
-                    riddle.riddleGiver();
-
-                }
-                else if (input == "5")
-                {
-                    RockPaperScissors rps = new RockPaperScissors();
-                    rps.RPS();
-                }
-                else if (input == "6")
-                {
-                    RandomFactGiver fact = new RandomFactGiver();
-                    fact.factGiver();
-                }
-                else if (input == "7")
-                {
-                    RageInducingRockPaperScissors rps = new RageInducingRockPaperScissors();
-                    rps.RIRPS();
-                }
-                else
-                {
-                    Console.Clear();
-
-                    continue;
+                    inputLine = sr.ReadLine();
+                    Console.WriteLine(inputLine);
                 }
             }
-
-            Console.Clear();
-            Credits firstCredits = new Credits();
-            Console.WriteLine($"{firstCredits.Contributers()}");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Press [Enter] to exit the Great Flambini");
-
             Console.ReadLine();
-        }
+            Console.Clear();
+                        bool menu = true;
+
+                        while (menu == true)
+                        {
+                            Console.WriteLine("****************************************************************************************************************************************************************************");
+                            Console.WriteLine("\n****************************************************************************************************************************************************************************");
+                            Console.WriteLine("\n****************************************************************************************************************************************************************************");
+                            Console.Write(
+                                "\t\t\t\t\t\t\t1) Fortune Teller" +
+                                "\n\t\t\t\t\t\t\t2) Magic 8-Ball (SFW)" +
+                                "\n\t\t\t\t\t\t\t3) Magic 8-Ball (NSFW)" +
+                                "\n\t\t\t\t\t\t\t4) Riddle Giver" +
+                                "\n\t\t\t\t\t\t\t5) Rock Paper Scissors" +
+                                "\n\t\t\t\t\t\t\t6) Random Fact Giver" +
+                                "\n\t\t\t\t\t\t\t7) Paper Rock Scissors" +
+                                "\n\t\t\t\t\t\t\tQ) Quit" +
+                                "\n\n\n\t\t\t\t\t\t\tPlease choose an option: ");
+                            String input = Console.ReadLine().ToLower().Trim();
+
+                            if (input.Length == 0)
+                            {
+                                Console.Clear();
+                                continue;
+
+                            }
+                            if (input.Length > 1)
+                            {
+                                Console.Clear();
+                                continue;
+
+                            }
+                            input = input.Substring(0, 1);
+
+                            if (input == "q")
+                            {
+                                menu = false;
+                                break;
+                            }
+                            else if (input == "1")
+                            {
+                                FortuneTeller fortuneTeller = new FortuneTeller();
+
+                                fortuneTeller.fortuneTeller();
+                            }
+                            else if (input == "2")
+                            {
+                                MagicEightBall_SFW_ magicEightBallSFW = new MagicEightBall_SFW_();
+
+                                magicEightBallSFW.magicEightBallSFW();
+                            }
+                            else if (input == "3")
+                            {
+                                MagicEightBall_NSFW_ magicEightBallNSFW = new MagicEightBall_NSFW_();
+
+                                magicEightBallNSFW.magicEightBallNSFW();
+
+                            }
+                            else if (input == "4")
+                            {
+                                Riddles riddle = new Riddles();
+
+                                riddle.riddleGiver();
+
+                            }
+                            else if (input == "5")
+                            {
+                                RockPaperScissors rps = new RockPaperScissors();
+                                rps.RPS();
+                            }
+                            else if (input == "6")
+                            {
+                                RandomFactGiver fact = new RandomFactGiver();
+                                fact.factGiver();
+                            }
+                            else if (input == "7")
+                            {
+                                RageInducingRockPaperScissors rps = new RageInducingRockPaperScissors();
+                                rps.RIRPS();
+                            }
+                            else
+                            {
+                                Console.Clear();
+
+                                continue;
+                            }
+                        }
+
+                        Console.Clear();
+                        Credits firstCredits = new Credits();
+                        Console.WriteLine($"{firstCredits.Contributers()}");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Press [Enter] to exit the Great Flambini");
+
+                        Console.ReadLine();
+                    }
     }
 }
 
