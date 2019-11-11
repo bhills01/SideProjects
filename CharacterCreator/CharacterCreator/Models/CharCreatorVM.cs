7c7c995df187b373b1891b9a348f40ed4cb50605
Bradley <bradleyhills@gmail.com>
2019-11-11 15:01:20 -0500

    Starting on Character Creator

---
+++ CharacterCreator/CharacterCreator/Models/CharCreatorVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Models
+{
+    public class CharCreatorVM
+    {
+        public CharCreatorVM()
+        {
+
+        }
+        public CharCreatorVM(string gameUniverse)
+        {
+            gameUniverse = GameUniverse;
+        }
+        public string GameUniverse { get; set; }
+    }
+}
